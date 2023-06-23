a7fc1df17b7c6cff7c0d27b1ff9b0f2c571f195b
@hozan94 <hozan949@gmail.com>
2023-06-23 02:57:55 +0100

    chore: customising the interceptor's dto

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -10,6 +10,7 @@ import { map } from 'rxjs/operators';
 import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     // Run something before a request is handled by the request handler
     console.log('I am running before the handler', context);
@@ -18,7 +19,7 @@ export class SerializeInterceptor implements NestInterceptor {
       map((data: any) => {
         // Run something before the response is sent out
         console.log('I am running before the response is sent out', data);
-        return plainToInstance(UserDto, data, {
+        return plainToInstance(this.dto, data, {
           excludeExtraneousValues: true,
         });
       }),
