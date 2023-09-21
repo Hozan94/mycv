6e2cd1b01e43bbd0e0095245109f1744b4816c63
@hozan94 <hozan949@gmail.com>
2023-09-22 00:37:54 +0100

    test: add more testing

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -28,4 +28,24 @@ describe('Authentication System', () => {
         expect(email).toEqual(email);
       });
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+
+    //We get a cookie back from this request but it doesn't get handled by super-agent
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    //This pulls out the Set-cookie header where a cookie is sent back to us inside a response
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie);
+    expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
