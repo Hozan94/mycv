7685a6a91cb0a81b8710e6965a592170ff153761
@hozan94 <hozan949@gmail.com>
2023-10-09 01:30:35 +0100

    chore: add authorization guard

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -13,10 +13,15 @@ import { Report } from 'src/reports/report.entity';
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   email: string;
+
   @Column()
   password: string;
+
+  @Column({ default: true })
+  admin: boolean;
 
   @OneToMany(() => Report, (report) => report.user)
   reports: Report[];
